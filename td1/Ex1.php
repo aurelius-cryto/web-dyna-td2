c6b43c70ddc6b8f52fb71caedbf1f8c1fc747966
marty hughes <21908919@etu.unicaen.fr>
2020-03-18 13:04:51 +0100

    Ajout du tp2 intro

---
+++ td1/Ex1.php
@@ -0,0 +1,6 @@
+<a href="?message=hello">Afficher Hello</a>
+<a href="?message=bonjour">Afficher bonjour</a>
+<a href="?message=salut">Afficher salut</a>
+<?php
+echo $_GET["message"]; 
+echo ("l'ami");
