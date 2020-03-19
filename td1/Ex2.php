a0792e71b5895161bd0807d3f80a89cab0ca6f28
marty hughes <21908919@etu.unicaen.fr>
2020-03-19 17:37:05 +0100

    ex2

--- td1/Ex2.php
+++ td1/Ex2.php
@@ -1,4 +1,20 @@
-<a href="?message=hello">Afficher Hello</a>
 
-<?php
-echo $_GET["size"]
+
+	<h1>Teste</h1>
+
+<a href="?qui=nobody">Personne</a><br>
+<a href="?qui=World">Tout le monde</a><br>
+<a href="?qui=Moi">Moi</a><br>
+
+
+<form method="GET">
+    <label for="size">Size : </label>
+    <input type="number" value="" name="size" id="size">
+    <input type="char" value="" name="color" id="size">
+    <input type="submit" value="Valider">
+</form>
+
+<?php 
+$who=$_GET["qui"]??" personne";
+
+echo "<h2>Hello $who! </h2>";
