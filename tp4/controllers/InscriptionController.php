902c453acf1773fe983ddf3c5c063c5cd2e752e9
unknown <21914874@etu.unicaen.fr>
2020-05-13 09:51:01 +0200

    mon 2e commit tp4

---
+++ tp4/controllers/InscriptionController.php
@@ -0,0 +1,35 @@
+<?php
+namespace controllers;
+ /**
+ * 
+ **/
+ class InscriptionController extends ControllerBase{
+
+	/**
+	*
+	
+	*/
+	public function index(){
+		$this->loadView("InscriptionController/index.html");
+	}
+
+	/**
+	 *
+	**/
+	public function bouton(){
+		
+		$this->loadView('InscriptionController/bouton.html');
+
+	}
+
+
+	/**
+	 
+	**/
+	public function get($name,$addrmel){
+		
+		$this->loadView('InscriptionController/get.html',['name'=>$name, 'addrmel'=>$addrmel]);
+
+	}
+
+}
